2675b7a8608fb0ecb0e3fe6303f66729dafb2186
18032391370 <18032391370@163.com>
2025-05-07 08:06:41 +0800

    chore: 加注释

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,9 +1,11 @@
 export function timeFix () {
+  // 时间问候函数
   const time = new Date()
   const hour = time.getHours()
   return hour < 9 ? '早上好' : hour <= 11 ? '上午好' : hour <= 13 ? '中午好' : hour < 20 ? '下午好' : '晚上好'
 }
 
+// 提示语
 export function welcome () {
   const arr = ['休息一会儿吧', '准备吃什么呢?', '要不要打一把 DOTA', '我猜你可能累了']
   const index = Math.floor(Math.random() * arr.length)
@@ -13,26 +15,28 @@ export function welcome () {
 /**
  * 触发 window.resize
  */
+// 在窗口大小改变时触发事件
 export function triggerWindowResizeEvent () {
-  const event = document.createEvent('HTMLEvents')
-  event.initEvent('resize', true, true)
+  const event = document.createEvent('HTMLEvents') // 创建一个新的HTML事件
+  event.initEvent('resize', true, true) // 初始化事件 类型为resize 设置事件冒泡、取消
   event.eventType = 'message'
-  window.dispatchEvent(event)
+  window.dispatchEvent(event) // 触发所有监听resize事件的回调函数
 }
 
 export function handleScrollHeader (callback) {
+  // 监听滚动事件并判断滚动方向 停止滚动后调用回调函数并传入滚动方向
   let timer = 0
 
-  let beforeScrollTop = window.pageYOffset
-  callback = callback || function () {}
-  window.addEventListener(
+  let beforeScrollTop = window.pageYOffset // 记录上一次滚动的位置
+  callback = callback || function () {} // 设置回调函数默认为空函数
+  window.addEventListener( // 监听滚动事件
     'scroll',
     event => {
-      clearTimeout(timer)
-      timer = setTimeout(() => {
+      clearTimeout(timer) // 清除上一次的定时器
+      timer = setTimeout(() => { // 设置定时器
         let direction = 'up'
         const afterScrollTop = window.pageYOffset
-        const delta = afterScrollTop - beforeScrollTop
+        const delta = afterScrollTop - beforeScrollTop // 计算本次和上次滚动的位置差值判断滚动方向
         if (delta === 0) {
           return false
         }
@@ -45,11 +49,12 @@ export function handleScrollHeader (callback) {
   )
 }
 
+// 用于判断用户浏览器是否为ie浏览器
 export function isIE () {
-  const bw = window.navigator.userAgent
+  const bw = window.navigator.userAgent // 获取浏览器的用户代理信息
   const compare = (s) => bw.indexOf(s) >= 0
-  const ie11 = (() => 'ActiveXObject' in window)()
-  return compare('MSIE') || ie11
+  const ie11 = (() => 'ActiveXObject' in window)() // 单独检测IE11浏览器
+  return compare('MSIE') || ie11 // 检查用户代理
 }
 
 /**
@@ -58,37 +63,39 @@ export function isIE () {
  * @param timeout
  */
 export function removeLoadingAnimate (id = '', timeout = 1500) {
-  if (id === '') {
+  // 移除页面上的加载动画
+  if (id === '') { // 传入元素ID 默认值为空字符串
     return
   }
-  setTimeout(() => {
+  setTimeout(() => { // 设置延迟操作
     document.body.removeChild(document.getElementById(id))
-  }, timeout)
+  }, timeout) // 延迟删除的时间
 }
-export function scorePassword (pass) {
+export function scorePassword (pass) { // 计算密码的强度
   let score = 0
   if (!pass) {
     return score
   }
   // award every unique letter until 5 repetitions
-  const letters = {}
+  const letters = {} // 记录密码中每个字符的出现次数
   for (let i = 0; i < pass.length; i++) {
       letters[pass[i]] = (letters[pass[i]] || 0) + 1
       score += 5.0 / letters[pass[i]]
   }
 
   // bonus points for mixing it up
-  const variations = {
+  const variations = { // 记录密码是否包含数字、小写字母、大写字母和非字母字符
       digits: /\d/.test(pass),
       lower: /[a-z]/.test(pass),
       upper: /[A-Z]/.test(pass),
       nonWords: /\W/.test(pass)
   }
 
-  let variationCount = 0
+  let variationCount = 0 // 统计密码中不同种类的字符类型
   for (var check in variations) {
       variationCount += (variations[check] === true) ? 1 : 0
   }
+  // 最终通过字符种类和字符重复次数的组合计算一个强度分数
   score += (variationCount - 1) * 10
 
   return parseInt(score)
